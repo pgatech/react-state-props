a012bca3db1511091b008120a582ba0cfc0a98f5
pgatech <prayogahakimm@gmail.com>
2022-12-07 17:15:13 +0700

    React Router DOM

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,15 @@
-import React,{Fragment} from "react";
+import React from "react";
 import GridCardPost from "../pages/GridCardPost";
 import NavbarHeader from "../pages/Navbar";
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 class Home extends React.Component {
     render(){
         return(
-            <Fragment>
+            <BrowserRouter>
                 <NavbarHeader />
                 <GridCardPost />
-            </Fragment>
+            </BrowserRouter>
         )
     }
 }
